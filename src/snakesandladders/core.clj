a677f06b19cb1e8f8c9d0da139e2b7a77698f985
Danurai <Danurai@users.noreply.github.com>
2017-12-29 14:20:26 +0000

    Add defn snakeorladder? and User Prompt info

--- src/snakesandladders/core.clj
+++ src/snakesandladders/core.clj
@@ -1,30 +1,30 @@
 (ns snakesandladders.core
   (:gen-class))
-
-(def sandl-map 
-"Map of Snakes/Chutes and Ladders on the board"
-  {4  14
-  9  31
-  16  6
-  21  42
-  20  38
-  28  84
-  36  44
-  47  26
-  49  11
-  51  67
-  56  53
-  62  19
-  64  60
-  71  91
-  80  100
-  87  24
-  93  73
-  95  75
-  98  78})
   
 (def game
-  {:turn 0 :players [] :spaces 100 :board {}})
+"Default game map including spaces on the board and map of snakes and ladders"
+  {:turn 0 
+   :players [] 
+	:spaces 100 
+	:sandl {4  14
+          9  31
+          16  6
+          21  42
+          20  38
+          28  84
+          36  44
+          47  26
+          49  11
+          51  67
+          56  53
+          62  19
+          64  60
+          71  91
+          80  100
+          87  24
+          93  73
+          95  75
+          98  78}})
  
 (def player
   {:name "" :space 0})
@@ -40,11 +40,19 @@
   [game]
   (rem (:turn game) (count (:players game))))
 
+(defn snakeorladder?
+"Check the map for the head of a snake :snake or the bottom of a ladder :ladder or returns :none"
+  [sandl-map space]
+  (cond
+    (> (sandl-map space space) space) :ladder
+    (< (sandl-map space space) space) :snake
+	 :else :none))
+
 (defn print-interface
 "Output the current game state"
   [game]
-  (println (str "\nTurn: " (:turn game)))
-  (doseq [p (:players game)] (println (str "Player " (:name p) " is on space " (:space p))))  
+  (doseq [p (:players game)] (println (str (:name p) " is on space " (:space p)))) 
+  game
 )
 
 (defn check-win
@@ -62,13 +70,14 @@
   [game]
   (let [player-id (current-player game)
        space (get-in game [:players player-id :space])]
+		 (println (str "Moving to space " space))
 		 (cond
-		   (< ((:board game) space space) space) (println (str "You landed on a snake, back to " ((:board game) space)))
-			(> ((:board game) space space) space) (println (str "You landed on a ladder, ascend to " ((:board game) space))))
-		 (assoc-in game [:players player-id :space] ((:board game) space space))))
+		   (= (snakeorladder? (:sandl game) space) :snake) (println (str "You landed on a snake, go back to " ((:sandl game) space) " :("))
+			(= (snakeorladder? (:sandl game) space) :ladder) (println (str "You landed on a ladder, climb up to " ((:sandl game) space) " :)")))
+		 (assoc-in game [:players player-id :space] ((:sandl game) space space))))
 
 (defn check-overshoot
-"Count back from 100"
+"Count back from end space"
   [game]
   (let [space (get-in game [:players (current-player game) :space])]
     (if (> space (:spaces game))
@@ -82,32 +91,50 @@
   [game]
   (let [roll (+ (rand-int 6) 1)
        player-id (current-player game)]
-   (println (str "You rolled a " roll "!"))
+   (println (str (get-in game [:players player-id :name]) " rolled a " roll "!"))
 	(update-in game [:players player-id :space] + roll)))
 		  
 (defn new-game
-"Create a new game with x players"
+"Create a new game with [x] 1-6 players."
+;; TODO get player names
   [x]
-  (-> game
-    (assoc :board sandl-map)
-    (assoc :players (into [] (map #( assoc-in player [:name] % ) (take x (iterate inc 1)))))))
+  {:pre [(integer? x)(>= x 1)(<= x 6)]}
+  (assoc game :players (into [] (map #( assoc-in player [:name] % ) (map #(inc %) (range 3))))))
 
-(defn player-turn
-"Do a player turn and then do another one if the game hasn't finished"
+(defn player-prompt
+"Prompt for user input"
   [game]
-  (print-interface game)
-  (let [user-input (prompt (#(str "\nPlayer " % " hit a key to roll, or type 'exit' to quit.") (:name ((:players game) (current-player game)))))]
+  (let [plyr (get-in game [:players (current-player game)])]
+    (str 
+      "\n" (:name plyr) "'s turn"
+      "\n----------"
+		"\nYou are on space " (:space plyr)
+		"\nComing up in the next 6 spaces: "
+      (apply str 
+        (map #(
+          let [sorl (snakeorladder? (:sandl game) %)]
+          (cond 
+            (= sorl :snake) (str % " Snake ")
+            (= sorl :ladder) (str % " Ladder ")
+				:else (str % " x ")))
+          (range (inc (:space plyr)) (min (+ (:space plyr) 7) 100))))
+		"\nHit enter to roll or type 'exit' to quit.")))
+ 
+(defn player-turn
+"Prompt player to roll the dice and move their piece"
+  [game]
+  (let [user-input (prompt (player-prompt game))]
     (if (= user-input "exit")
       :exit
       (-> game
           move-player
           check-overshoot
           check-map
-          (update-in [:turn] inc)
-          check-win))))
-
+          check-win
+			 print-interface
+          (update-in [:turn] inc)))))
 			 
-;; game-loop :players 2
+;; game-loop default :players 2
 (defn game-loop
 "Start a new game with default 2 players"
   [& {:keys [players]
